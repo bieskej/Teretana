f89edd3f3375427d03162f47cd0286460f2753f5
bieskej <bosko.raguz@fsre.sum.ba>
2024-08-12 15:42:31 +0200

    main Bosko

---
+++ src/main/java/org/example/TeretanaApplication.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeretanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeretanaApplication.class, args);
+    }
+
+}
