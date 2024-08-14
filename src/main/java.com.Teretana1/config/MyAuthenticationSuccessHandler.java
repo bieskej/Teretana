eb3fb71b6cdbe2c0d0bcb617e6f16d4c5fbae146
Petargitfreak <petar.raspudic@fsre.sum.ba>
2024-08-14 12:52:56 +0200

    Commited by Petar :)

---
+++ src/main/java.com.Teretana1/config/MyAuthenticationSuccessHandler.java
@@ -0,0 +1,28 @@
+package config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        // Preuzmite uloge korisnika
+        var authorities = authentication.getAuthorities();
+
+        // Logika preusmjeravanja na osnovu uloge
+        if (authorities.stream().anyMatch(a -> a.getAuthority().equals("ADMIN"))) {
+            response.sendRedirect("/templates/users");
+        } else if (authorities.stream().anyMatch(a -> a.getAuthority().equals("TRENER"))) {
+            response.sendRedirect("/trener");
+        } else if (authorities.stream().anyMatch(a -> a.getAuthority().equals("KLIJENT"))) {
+            response.sendRedirect("/klijent");
+        } else {
+            response.sendRedirect("/login");
+        }
+    }
+}
+
---
+++ src/main/java.com.Teretana1/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.teretana1.config;
+
+import com.teretana1.services.KorisnikDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    public KorisnikDetailsService korisnikDetailsService(){
+        return new KorisnikDetailsService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider=new DaoAuthenticationProvider();
+
+        daoAuthenticationProvider.setPasswordEncoder(this.passwordEncoder());
+
+        daoAuthenticationProvider.setUserDetailsService(this.korisnikDetailsService());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler(){
+        return new MyAuthenticationSuccessHandler();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/auth/register/**","/auth/register", "/WebLogo.png", "/auth/home", "/enduro1.jpg")
+                .permitAll()
+                .requestMatchers("/users/**").hasAuthority("ADMIN")
+                .requestMatchers("/supervisor/**").hasAuthority("SUPERVISOR")
+                .requestMatchers("/driver/**").hasAuthority("DRIVER")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin().successHandler(myAuthenticationSuccessHandler())
+                .loginPage("/auth/login")
+                .permitAll()
+                .usernameParameter("email")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/").permitAll();
+
+        http.authenticationProvider(authenticationProvider());
+        http.headers().frameOptions().sameOrigin();
+
+        http.exceptionHandling().authenticationEntryPoint((request, response, authException) ->
+                response.sendRedirect("/auth/home"));
+        return http.build();
+    }
+}
