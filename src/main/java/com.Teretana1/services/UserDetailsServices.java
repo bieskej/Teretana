8fd103505fd45a3302093cc07b1d2acbe1eeb960
bieskej <bosko.raguz@fsre.sum.ba>
2024-08-16 12:36:20 +0200

    main Bosko

---
+++ src/main/java/com.Teretana1/services/UserDetailsServices.java
@@ -0,0 +1,17 @@
+package com.Teretana1.services;
+
+import com.Teretana1.services.UserDetailsService;
+import com.Teretana1.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+    @Override
+    public com.Teretana1.models.UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User u = repository.findByEmail(username);
+        return new com.Teretana1.models.UserDetails(u);
+    }
+
+}
