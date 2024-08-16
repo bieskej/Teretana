59fe785884edf53baab1e93c67ef91e24a04a85c
bieskej <bosko.raguz@fsre.sum.ba>
2024-08-16 13:41:10 +0200

    main Bosko

---
+++ src/main/java/com.Teretana1/TeretanaApplication.java
@@ -0,0 +1,14 @@
+package com.Teretana1;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeretanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeretanaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com.Teretana1/controllers/TrenerController.java
@@ -0,0 +1,21 @@
+package com.Teretana1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/teacher")
+@PreAuthorize("hasAuthority('TEACHER')")
+public class TrenerController {
+
+    @GetMapping
+    public String teacherDashboard(Model model) {
+        // Dodajte potrebne atribute u model
+        return "teacher/dashboard";
+    }
+
+    // Ostale metode specifične za nastavnike
+}
